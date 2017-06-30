a1db5a98de7773506f1beb2aaafe8b98f57ee5ca
klvst3r <klvst3r@gmail.com>
2017-06-30 17:36:43 -0500

    Cierra sessión y valida url

--- vista/admin.php
+++ vista/admin.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 if(isset($_SESSION["usuario"]) && isset($_SESSION["pass"])){
-	echo "Bienvenido";
+	echo "Bienvenido<br/>";
+	echo "<a href='cerrarsesion.php'>Cerrar Sesión</a>";
 }else{
-	echo "<meta http-equip='refresh' content='0;url=index.php'";
+	echo "<meta http-equiv='refresh' content='0;url=index.php'>";
 } 
-
 ?>
